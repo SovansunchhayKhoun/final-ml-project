5a0b5355c01a2f4a16ddd004fc20dc9c376b7403
RethtihpongEM <rethtihpong@gmail.com>
2024-07-16 22:55:36 +0900

    using the linear regression from sklearn and svr from sklearn

--- nn_model.py
+++ nn_model.py
@@ -52,7 +52,7 @@ def train_fn(model: torch.nn.Module,
       # Calculate test loss
       test_loss = loss_fn(test_pred, test_label)
       
-    if epoch % (epoch / 10) == 0:
+    if epoch % 1000 == 0:
       print(f'Epoch: {epoch} | Train loss: {loss} | Test loss: {test_loss}')
   return loss, test_loss
   
@@ -66,12 +66,12 @@ def plot_pred(actual_value: torch.tensor, pred: torch.tensor):
   fig, axs = plt.subplots(2)
 
   # Plot actual target on first row
-  axs[0].scatter(actual_value.detach().numpy(), actual_value.detach().numpy(), c=actual_target_color, label='Actual Target')
+  axs[0].scatter(actual_value.detach().numpy(), actual_value.detach().numpy(), c=actual_target_color, label='Actual Target', alpha=0.5)
   axs[0].set_title('Actual Target')
   axs[0].legend()
 
   # Plot predicted target on second row
-  axs[1].scatter(actual_value.detach().numpy(), pred.detach().numpy(), c=predicted_target_color, label='Predicted Target')
+  axs[1].scatter(actual_value.detach().numpy(), pred.detach().numpy(), c=predicted_target_color, label='Predicted Target', alpha=0.5)
   axs[1].set_title('Predicted Target')
   axs[1].legend()
 
